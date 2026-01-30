camunda-community-hub/camunda-platform-7-mockito

Feature/517 auto mock throw event (#534)
* hub uses renovate, so drop local dependebot

* support intermediateThrowEvent in autoMock(), fixes #517

src/main/java/org/camunda/community/mockito/function/ParseDelegateExpressions.java
@@ -1,24 +1,21 @@
 package org.camunda.community.mockito.function;
 
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-import static org.camunda.bpm.model.bpmn.impl.BpmnModelConstants.CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION;
-import static org.camunda.bpm.model.bpmn.impl.BpmnModelConstants.CAMUNDA_ELEMENT_EXECUTION_LISTENER;
-import static org.camunda.bpm.model.bpmn.impl.BpmnModelConstants.CAMUNDA_ELEMENT_TASK_LISTENER;
-
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-
 import org.apache.commons.lang3.tuple.Pair;
 import org.camunda.community.mockito.DelegateExpressions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.camunda.bpm.model.bpmn.impl.BpmnModelConstants.*;
 
 /**
  * Parses a given BPMN File and returns a Set of all delegateExpression names.
@@ -27,7 +24,7 @@ import org.w3c.dom.NodeList;
  */
 public class ParseDelegateExpressions implements Function<URL, List<Pair<ParseDelegateExpressions.ExpressionType, String>>> {
 
-  public static enum ExpressionType {
+  public enum ExpressionType {
     EXECUTION_LISTENER(CAMUNDA_ELEMENT_EXECUTION_LISTENER) {
       @Override
       public void registerMock(final String name) {
@@ -38,17 +35,21 @@ public class ParseDelegateExpressions implements Function<URL, List<Pair<ParseDe
       public void registerMock(final String name) {
         DelegateExpressions.registerTaskListenerMock(name);
       }
-    }, JAVA_DELEGATE("serviceTask") {
+    }, JAVA_DELEGATE(Set.of(BPMN_ELEMENT_SERVICE_TASK, BPMN_ELEMENT_MESSAGE_EVENT_DEFINITION)) {
       @Override
       public void registerMock(final String name) {
         DelegateExpressions.registerJavaDelegateMock(name);
       }
     };
 
-    private final String element;
+    private final Set<String> elements;
 
     ExpressionType(final String element) {
-      this.element = element;
+      this.elements = Set.of(element);
+    }
+
+    ExpressionType(final Set<String> elements) {
+      this.elements = elements;
     }
 
     public abstract void registerMock(final String name);
@@ -63,34 +64,56 @@ public class ParseDelegateExpressions implements Function<URL, List<Pair<ParseDe
     return isNotBlank(delegateExpression) ? delegateExpression.replaceAll(PATTERN_DELEGATE_EXPRESSION, "$1") : null;
   }
 
-  private final Logger logger = LoggerFactory.getLogger(getClass());
-  private final ReadXmlDocumentFromResource readXmlDocumentFromResource = new ReadXmlDocumentFromResource();
+  static Pair<ExpressionType, String> extractDelegateExpressionName(final Pair<ExpressionType, Node> pair) {
+    final String delegateExpression = pair.getRight().getTextContent();
+    return Pair.of(
+      pair.getLeft(),
+      isNotBlank(delegateExpression) ? delegateExpression.replaceAll(PATTERN_DELEGATE_EXPRESSION, "$1") : null
+    );
+  }
 
   @Override
   public List<Pair<ExpressionType, String>> apply(final URL bpmnResource) {
     final Element root = new ReadXmlDocumentFromResource().apply(bpmnResource).getDocumentElement();
 
-    return new ArrayList<Pair<ExpressionType, String>>() {
-      {
-        for (ExpressionType type : ExpressionType.values()) {
-          final NodeList nodes = root.getElementsByTagNameNS("*", type.element);
+    final List<Pair<ExpressionType, NodeList>> nodeListsPerType = Stream.of(ExpressionType.values())
+      .flatMap(type -> type.elements.stream().map(it -> root.getElementsByTagNameNS("*", it))
+        .map(it -> Pair.of(type, it)))
+      .toList();
 
-          for (int i = 0; i < nodes.getLength(); i++) {
-            final NamedNodeMap attributes = nodes.item(i).getAttributes();
-
-            // TODO: this is not nice, but I cannot get getNamedItemNS("*", ...) to work properly
-            Node delegateExpression = Optional
-              .ofNullable(attributes.getNamedItem(CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION))
-              .orElse(attributes.getNamedItem("camunda:"+CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION));
-
-            if (delegateExpression != null) {
-              add(Pair.of(type, extractDelegateExpressionName(delegateExpression.getTextContent())));
-            }
-          }
-
-        }
-      }
-    };
+    return nodeListsPerType.stream()
+      .flatMap(typeToNodeList -> streamOf(typeToNodeList.getRight()).map(node -> {
+        final NamedNodeMap attributes = node.getAttributes();
+        // TODO: this is not nice, but I cannot get getNamedItemNS("*", ...) to work properly
+        Node delegateExpression = Optional
+          .ofNullable(attributes.getNamedItem(CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION))
+          .orElse(attributes.getNamedItem("camunda:" + CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION));
+        return Pair.of(typeToNodeList.getLeft(), delegateExpression);
+      })).filter(it -> it.getRight() != null)
+      .map(ParseDelegateExpressions::extractDelegateExpressionName)
+      .toList();
   }
 
+  private static Stream<Node> streamOf(final NodeList nodeList) {
+    final long length = nodeList.getLength();
+    final Iterator<Node> iterator = new Iterator<>() {
+      private final AtomicInteger index = new AtomicInteger(0);
+
+      @Override
+      public boolean hasNext() {
+        return index.get() < length;
+      }
+
+      @Override
+      public Node next() {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+        return nodeList.item(index.getAndIncrement());
+      }
+    };
+    final Spliterator<Node> spliterator = Spliterators.spliterator(iterator, length, Spliterator.ORDERED);
+
+    return StreamSupport.stream(spliterator, false);
+  }
 }

cibseven-community-hub/cibseven-mockito

src/main/java/org/cibseven/community/mockito/function/ParseDelegateExpressions.java
@@ -1,15 +1,14 @@
 package org.cibseven.community.mockito.function;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
-import static org.cibseven.bpm.model.bpmn.impl.BpmnModelConstants.CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION;
-import static org.cibseven.bpm.model.bpmn.impl.BpmnModelConstants.CAMUNDA_ELEMENT_EXECUTION_LISTENER;
-import static org.cibseven.bpm.model.bpmn.impl.BpmnModelConstants.CAMUNDA_ELEMENT_TASK_LISTENER;
+import static org.cibseven.bpm.model.bpmn.impl.BpmnModelConstants.*;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.cibseven.community.mockito.DelegateExpressions;
@@ -27,7 +26,7 @@ import org.w3c.dom.NodeList;
  */
 public class ParseDelegateExpressions implements Function<URL, List<Pair<ParseDelegateExpressions.ExpressionType, String>>> {
 
-  public static enum ExpressionType {
+  public enum ExpressionType {
     EXECUTION_LISTENER(CAMUNDA_ELEMENT_EXECUTION_LISTENER) {
       @Override
       public void registerMock(final String name) {
@@ -38,17 +37,21 @@ public class ParseDelegateExpressions implements Function<URL, List<Pair<ParseDe
       public void registerMock(final String name) {
         DelegateExpressions.registerTaskListenerMock(name);
       }
-    }, JAVA_DELEGATE("serviceTask") {
+    }, JAVA_DELEGATE(Set.of(BPMN_ELEMENT_SERVICE_TASK, BPMN_ELEMENT_MESSAGE_EVENT_DEFINITION)) {
       @Override
       public void registerMock(final String name) {
         DelegateExpressions.registerJavaDelegateMock(name);
       }
     };
 
-    private final String element;
+    private final Set<String> elements;
 
     ExpressionType(final String element) {
-      this.element = element;
+      this.elements = Set.of(element);
+    }
+
+    ExpressionType(final Set<String> elements) {
+      this.elements = elements;
     }
 
     public abstract void registerMock(final String name);
@@ -63,34 +66,56 @@ public class ParseDelegateExpressions implements Function<URL, List<Pair<ParseDe
     return isNotBlank(delegateExpression) ? delegateExpression.replaceAll(PATTERN_DELEGATE_EXPRESSION, "$1") : null;
   }
 
-  private final Logger logger = LoggerFactory.getLogger(getClass());
-  private final ReadXmlDocumentFromResource readXmlDocumentFromResource = new ReadXmlDocumentFromResource();
+  static Pair<ExpressionType, String> extractDelegateExpressionName(final Pair<ExpressionType, Node> pair) {
+    final String delegateExpression = pair.getRight().getTextContent();
+    return Pair.of(
+      pair.getLeft(),
+      isNotBlank(delegateExpression) ? delegateExpression.replaceAll(PATTERN_DELEGATE_EXPRESSION, "$1") : null
+    );
+  }
 
   @Override
   public List<Pair<ExpressionType, String>> apply(final URL bpmnResource) {
     final Element root = new ReadXmlDocumentFromResource().apply(bpmnResource).getDocumentElement();
 
-    return new ArrayList<Pair<ExpressionType, String>>() {
-      {
-        for (ExpressionType type : ExpressionType.values()) {
-          final NodeList nodes = root.getElementsByTagNameNS("*", type.element);
+    final List<Pair<ExpressionType, NodeList>> nodeListsPerType = Stream.of(ExpressionType.values())
+      .flatMap(type -> type.elements.stream().map(it -> root.getElementsByTagNameNS("*", it))
+        .map(it -> Pair.of(type, it)))
+      .toList();
 
-          for (int i = 0; i < nodes.getLength(); i++) {
-            final NamedNodeMap attributes = nodes.item(i).getAttributes();
-
-            // TODO: this is not nice, but I cannot get getNamedItemNS("*", ...) to work properly
-            Node delegateExpression = Optional
-              .ofNullable(attributes.getNamedItem(CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION))
-              .orElse(attributes.getNamedItem("camunda:"+CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION));
-
-            if (delegateExpression != null) {
-              add(Pair.of(type, extractDelegateExpressionName(delegateExpression.getTextContent())));
-            }
-          }
-
-        }
-      }
-    };
+    return nodeListsPerType.stream()
+      .flatMap(typeToNodeList -> streamOf(typeToNodeList.getRight()).map(node -> {
+        final NamedNodeMap attributes = node.getAttributes();
+        // TODO: this is not nice, but I cannot get getNamedItemNS("*", ...) to work properly
+        Node delegateExpression = Optional
+          .ofNullable(attributes.getNamedItem(CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION))
+          .orElse(attributes.getNamedItem("camunda:" + CAMUNDA_ATTRIBUTE_DELEGATE_EXPRESSION));
+        return Pair.of(typeToNodeList.getLeft(), delegateExpression);
+      })).filter(it -> it.getRight() != null)
+      .map(ParseDelegateExpressions::extractDelegateExpressionName)
+      .toList();
   }
 
+  private static Stream<Node> streamOf(final NodeList nodeList) {
+    final long length = nodeList.getLength();
+    final Iterator<Node> iterator = new Iterator<>() {
+      private final AtomicInteger index = new AtomicInteger(0);
+
+      @Override
+      public boolean hasNext() {
+        return index.get() < length;
+      }
+
+      @Override
+      public Node next() {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+        return nodeList.item(index.getAndIncrement());
+      }
+    };
+    final Spliterator<Node> spliterator = Spliterators.spliterator(iterator, length, Spliterator.ORDERED);
+
+    return StreamSupport.stream(spliterator, false);
+  }
 }